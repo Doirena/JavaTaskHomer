2d6f09df0ba176d92f7b0b658c570208a4e3e35d
Doreina <barkauskaite.dovile@gmail.com>
2020-07-01 12:04:23 +0300

    All data move on H2 database inmemory

---
+++ springbootrest/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO owner (name) VALUES ('Anna');
+INSERT INTO owner (name) VALUES ('Ben');
+INSERT INTO owner (name) VALUES ('Victoria');
+INSERT INTO owner (name) VALUES ('John');
+INSERT INTO property (type, tax_rate ) VALUES ('Flat', 2.5);
+INSERT INTO property (type, tax_rate ) VALUES ('Garden House', 4.5);
+INSERT INTO property (type, tax_rate ) VALUES ('Apartment', 11.25);
+INSERT INTO property (type, tax_rate ) VALUES ('House', 20.75);
+INSERT INTO building_records(address, owner_id, size, value, property_type_id) VALUES('Naugardukas str. 1, Vilnius', 1, 38, 20, 1);
+INSERT INTO building_records(address, owner_id, size, value, property_type_id) VALUES('Vilnius str. 11, Vilnius', 1, 200, 2.2, 4);
+INSERT INTO building_records(address, owner_id, size, value, property_type_id) VALUES('Vilnius str. 21, Vilnius', 2, 21, 3.2, 3);
+INSERT INTO building_records(address, owner_id, size, value, property_type_id) VALUES('Vilnius str. 41, Vilnius', 3, 100, 5.2, 2);
+INSERT INTO building_records(address, owner_id, size, value, property_type_id) VALUES('Birute str. 21, Vilnius', 4, 78, 6.2, 1);
+INSERT INTO building_records(address, owner_id, size, value, property_type_id) VALUES('Vytauto str. 51, Vilnius', 1, 178, 4.2, 2);
